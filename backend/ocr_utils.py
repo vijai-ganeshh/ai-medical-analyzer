01abc5b342ebb79d94923fc971c9ac25132a7628
vijai-ganeshh <jvganeshh1@gmail.com>
2026-05-25 21:56:23 +0530

    Fixed deployment requirementoo

--- backend/ocr_utils.py
+++ backend/ocr_utils.py
@@ -1,20 +1,12 @@
 import fitz
-import easyocr
-
-reader = easyocr.Reader(['en'])
 
 def extract_text_from_pdf(pdf_path):
 
-    extracted_text = ""
+    text = ""
 
     pdf_document = fitz.open(pdf_path)
 
-    for page_number in range(len(pdf_document)):
+    for page in pdf_document:
+        text += page.get_text()
 
-        page = pdf_document.load_page(page_number)
-
-        text = page.get_text()
-
-        extracted_text += text
-
-    return extracted_text
+    return text
